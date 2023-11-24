bfe5954fc284aaae692ca402776257c796f25ec7
Steve515 <stevewu24@gmail.com>
2023-11-24 02:00:51 -0800

    added boiler plates and removed default layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        white: "#fff",
+        turquoise: "#63e2c6",
+        aqua: "#6ef9f5",
+        mediumseagreen: "#399e5a",
+      },
+      fontFamily: {
+        itim: "Itim",
+        inter: "Inter",
+      },
     },
   },
   plugins: [],
